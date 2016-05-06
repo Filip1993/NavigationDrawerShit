c2d7abff49de88fc13e705607ea294b53dfd2810
Filip <filip.kesteli@gmail.com>
2016-05-06 23:25:27 +0200

    Initial commit

---
+++ app/src/main/java/com/filipkesteli/navigationdrawershit/EveningAskActivity.java
@@ -0,0 +1,13 @@
+package com.filipkesteli.navigationdrawershit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EveningAskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evening_ask);
+    }
+}
---
+++ app/src/main/java/com/filipkesteli/navigationdrawershit/ProbniActivity.java
@@ -0,0 +1,13 @@
+package com.filipkesteli.navigationdrawershit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProbniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_probni);
+    }
+}
